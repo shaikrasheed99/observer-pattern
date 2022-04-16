6742c91ced93bae250b2fa36367f2e6b9970aa3e
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-16 11:32:15 +0530

    Refactor introduce new method in channel to check existance of subscriber

--- src/main/java/com/tw/youtube/Channel.java
+++ src/main/java/com/tw/youtube/Channel.java
@@ -20,13 +20,13 @@ public class Channel implements IPublisher {
 
     @Override
     public void subscribe(ISubscriber subscriber) throws SubscriberAlreadyExist {
-        if (subscribers.contains(subscriber)) throw new SubscriberAlreadyExist();
+        if (isExist(subscriber)) throw new SubscriberAlreadyExist();
         subscribers.add(subscriber);
     }
 
     @Override
     public void unsubscribe(ISubscriber subscriber) throws SubscriberDoesNotExist {
-        if (!subscribers.contains(subscriber)) throw new SubscriberDoesNotExist();
+        if (!isExist(subscriber)) throw new SubscriberDoesNotExist();
         subscribers.remove(subscriber);
     }
 
@@ -38,5 +38,9 @@ public class Channel implements IPublisher {
     public void upload(Video video) {
         videos.add(video);
         sendNotifications();
+    }
+
+    private boolean isExist(ISubscriber subscriber) {
+        return subscribers.contains(subscriber);
     }
 }
